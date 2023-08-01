3f41e89a47c20d08fa62cb8eedf44712d34b163c
Guillermo <guillef33@gmail.com>
2023-08-01 12:16:13 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "prueba.local",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,49 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Image from "next/image";
 
-const inter = Inter({ subsets: ['latin'] })
+async function getData() {
+  const res = await fetch("http://prueba.local/wp-json/wp/v2/posts");
 
-export default function Home() {
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+async function getImages() {
+  const res = await fetch("http://prueba.local/wp-json/wp/v2/media");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+  const images = await getImages();
+
+  console.log(data);
+  console.log(images);
+
   return (
-    <main className={styles.main}>
-      <
+    <main className="grid grid-cols-3 gap-4">
+      {data.map((item, index) => {
+        return (
+          <div className="">
+            <div>
+              {images.map((imageItem, imageIndex) => {
+                if (item.id === imageItem.post) {
+                  return <img src={imageItem.source_url} />;
+                }
+              })}
+            </div>
+            <h1 className="text-[2rem]">{item.title.rendered}</h1>
+            <p>{item.content.rendered}</p>
+          </div>
+        );
+      })}
     </main>
-  )
+  );
 }

d18c8984066f369af959cad583537b3ef45d4679
Guillermo <guillef33@gmail.com>
2023-09-12 10:17:08 -0300

    i18n and redux

---
+++ src/app/components/pokemon/pokemon.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Pokemon() {
+  return <div>Nuestra lista de pokemones</div>;
+}
+
+export default Pokemon;
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,32 @@
+"use client";
+
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Link from "next/link";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import enTranslaton from "../app/translations/en.json";
+import Pokemon from "./components/pokemon/pokemon";
 
-export default async function Home() {
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: enTranslaton,
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default function Home() {
+  const { t } = useTranslation();
+
   return (
     <main className="max-w-screen-xl mx-auto">
-      <h1>Esta es la home, si queres ver algo anda al blog</h1>
+      <h1>{t("Bienvenida")}</h1>
+      <h2>{t("Welcome to React")}</h2>
+      <Pokemon />
     </main>
   );
 }
